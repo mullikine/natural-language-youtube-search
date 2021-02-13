e430b5de0c03fc995529659b5b3c54aefc7e8843
Shane Mulligan <mullikine@gmail.com>
2021-02-14 00:37:15 +1300

    Sun 14 Feb 2021 00:37:15 NZDT

--- natural-language-youtube-search.py
+++ natural-language-youtube-search.py
@@ -127,9 +127,9 @@ def search_video(search_query, display_heatmap=True, display_results_count=3):
     print()
   myembed(globals(), locals())
 
-  # Display the top 3 frames
-  for frame_id in best_photo_idx:
-    display(video_frames[frame_id])
-    print()
+  # # Display the top 3 frames
+  # for frame_id in best_photo_idx:
+  #   display(video_frames[frame_id])
+  #   print()
 
 search_video("a stop sign")
